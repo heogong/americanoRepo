fafee733659768bbc4d83756f189c0e48846a23e
DESKTOP-ASVRCK1\JJ <heogong79@gmail.com>
2018-06-15 16:43:36 +0900

    신규 프로젝트 생성

---
+++ foundation/src/main/java/com/issac/foundation/FoundationApplication.java
@@ -0,0 +1,12 @@
+package com.issac.foundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundationApplication.class, args);
+	}
+}
